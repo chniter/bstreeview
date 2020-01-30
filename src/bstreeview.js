ddad92030172e924ef662353269235b1f16af3f4
chniter <sami.chniter@gmail.com>
2020-01-30 18:55:24 +0100

    Another stuff.

--- src/bstreeview.js
+++ src/bstreeview.js
@@ -5,56 +5,57 @@
     // Create the defaults once
     var pluginName = "bstreeview",
         defaults = {
-            propertyName: "value"
+            expandIcon: 'fa fa-angle-down',
+            collapseIcon: 'fa fa-angle-right'
         };
 
-    defaults.settings = {
-
-        injectStyle: true,
-
-        levels: 2,
-
-        expandIcon: 'glyphicon glyphicon-plus',
-        collapseIcon: 'glyphicon glyphicon-minus',
-        emptyIcon: 'glyphicon',
-        nodeIcon: '',
-        selectedIcon: '',
-        checkedIcon: 'glyphicon glyphicon-check',
-        uncheckedIcon: 'glyphicon glyphicon-unchecked'
-    };
     var templates = {
-        treeview: '<div class="bstreeview list-group list-group-root card"></div>',
-        treeviewItem: '<div href="#item-1" class="list-group-item" data-toggle="collapse"></div>',
-        treeviewGroupItem: '<div class="list-group collapse" id="item-1-1"></div>'
+        treeview: '<div class="bstreeview"></div>',
+        treeviewItem: '<div href="#itemid" class="list-group-item" data-toggle="collapse"></div>',
+        treeviewGroupItem: '<div class="list-group collapse" id="itemid"></div>',
+        treeviewItemStateIcon: '<i class="state-icon"></i>'
     };
-    // The actual plugin constructor
+    /**
+     * BsTreeview Plugin constructor.
+     * @param {*} element 
+     * @param {*} options 
+     */
     function bstreeView(element, options) {
         this.element = element;
         this.settings = $.extend({}, defaults, options);
-        this._defaults = defaults;
-        this._name = pluginName;
         this.init();
     }
 
-    // Avoid Plugin.prototype conflicts
+    /**
+     * Avoid plugin conflict.
+     */
     $.extend(bstreeView.prototype, {
         init: function () {
             this.tree = [];
             this.nodes = [];
             if (this.settings.data) {
                 this.settings.data = $.parseJSON(this.settings.data);
-
                 this.tree = $.extend(true, [], this.settings.data);
                 delete this.settings.data;
             }
-            this.settings = $.extend({}, this._defaults.settings, this.settings);
-
+            //this.settings = $.extend({}, defaults, this.settings);
 
             $(this.element).addClass('bstreeview list-group list-group-root card');
             this.initData({ nodes: this.tree }, 0);
             this.build($(this.element), this.tree, 0);
-        },
+            // Update angle icon on collapse
+            $('.bstreeview').on('click', '.list-group-item', function () {
+                $('.state-icon', this)
+                    .toggleClass('fa-angle-right')
+                    .toggleClass('fa-angle-down');
+            });
 
+        },
+        /**
+         * Initialize treeview Data.
+         * @param {*} node 
+         * @param {*} level 
+         */
         initData: function (node, level) {
             if (!node.nodes) return;
             level += 1;
@@ -72,7 +73,12 @@
                 }
             });
         },
-
+        /**
+         * Build treeview.
+         * @param {*} parentElement 
+         * @param {*} nodes 
+         * @param {*} depth 
+         */
         build: function (parentElement, nodes, depth) {
             var leftPadding = "1.25rem;";
             if (depth > 0) {
@@ -83,10 +89,16 @@
             var _this = this;
 
             $.each(nodes, function addNodes(id, node) {
-                
+
                 var treeItem = $(templates.treeviewItem)
                     .attr('href', '#item-' + node.nodeId)
-                    .attr('style', 'padding-left:'+leftPadding);
+                    .attr('style', 'padding-left:' + leftPadding);
+                if (node.nodes) {
+                    var treeItemStateIcon = $(templates.treeviewItemStateIcon)
+                        .addClass(_this.settings.collapseIcon);
+                    treeItem.append(treeItemStateIcon);
+                }
+
                 treeItem.append(node.text);
 
                 parentElement.append(treeItem);
