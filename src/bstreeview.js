52e4f71de80ae143a159fe7ceb78e3ceb5e8d2eb
chniter <sami.chniter@gmail.com>
2020-01-31 18:45:56 +0100

    Now, it work with icones!

--- src/bstreeview.js
+++ src/bstreeview.js
@@ -1,19 +1,34 @@
+/*! @preserve
+ * bstreeview.js
+ * Version: 0.0.1
+ * Authors: Sami CHNITER <sami.chniter@gmail.com>
+ * Copyright 2020
+ * License: Apache License 2.0
+ *
+ * Project: https://github.com/chniter/bstreeview
+ */
 ; (function ($, window, document, undefined) {
 
     "use strict";
 
-    // Create the defaults once
+    /**
+     * Default bstreeview  options.
+     */
     var pluginName = "bstreeview",
         defaults = {
             expandIcon: 'fa fa-angle-down',
-            collapseIcon: 'fa fa-angle-right'
+            collapseIcon: 'fa fa-angle-right',
+            indent: 1.25
         };
-
+    /**
+     * bstreeview HTML templates.
+     */
     var templates = {
         treeview: '<div class="bstreeview"></div>',
         treeviewItem: '<div href="#itemid" class="list-group-item" data-toggle="collapse"></div>',
         treeviewGroupItem: '<div class="list-group collapse" id="itemid"></div>',
-        treeviewItemStateIcon: '<i class="state-icon"></i>'
+        treeviewItemStateIcon: '<i class="state-icon"></i>',
+        treeviewItemIcon: '<i class="item-icon"></i>'
     };
     /**
      * BsTreeview Plugin constructor.
@@ -22,6 +37,7 @@
      */
     function bstreeView(element, options) {
         this.element = element;
+        this.itemIdPrefix = element.id + "-item-";
         this.settings = $.extend({}, defaults, options);
         this.init();
     }
@@ -33,33 +49,32 @@
         init: function () {
             this.tree = [];
             this.nodes = [];
+            // Retrieve bstreeview Json Data.
             if (this.settings.data) {
                 this.settings.data = $.parseJSON(this.settings.data);
                 this.tree = $.extend(true, [], this.settings.data);
                 delete this.settings.data;
             }
-            //this.settings = $.extend({}, defaults, this.settings);
+            // Set main bstreeview class to element.
+            $(this.element).addClass('bstreeview');
 
-            $(this.element).addClass('bstreeview list-group list-group-root card');
-            this.initData({ nodes: this.tree }, 0);
+            this.initData({ nodes: this.tree });
+            var _this = this;
             this.build($(this.element), this.tree, 0);
             // Update angle icon on collapse
             $('.bstreeview').on('click', '.list-group-item', function () {
                 $('.state-icon', this)
-                    .toggleClass('fa-angle-right')
-                    .toggleClass('fa-angle-down');
+                    .toggleClass(_this.settings.expandIcon)
+                    .toggleClass(_this.settings.collapseIcon);
             });
 
         },
         /**
          * Initialize treeview Data.
          * @param {*} node 
-         * @param {*} level 
          */
-        initData: function (node, level) {
+        initData: function (node) {
             if (!node.nodes) return;
-            level += 1;
-
             var parent = node;
             var _this = this;
             $.each(node.nodes, function checkStates(index, node) {
@@ -69,7 +84,7 @@
                 _this.nodes.push(node);
 
                 if (node.nodes) {
-                    _this.initData(node, level);
+                    _this.initData(node);
                 }
             });
         },
@@ -80,32 +95,41 @@
          * @param {*} depth 
          */
         build: function (parentElement, nodes, depth) {
+            var _this = this;
+            // Calculate item padding.
             var leftPadding = "1.25rem;";
             if (depth > 0) {
-                leftPadding = (1.25 + depth * 1.25).toString() + "rem;";
+                leftPadding = (_this.settings.indent + depth * _this.settings.indent).toString() + "rem;";
             }
 
             depth += 1;
-            var _this = this;
-
+            // Add each node and sub-nodes.
             $.each(nodes, function addNodes(id, node) {
-
+                // Main node element.
                 var treeItem = $(templates.treeviewItem)
-                    .attr('href', '#item-' + node.nodeId)
+                    .attr('href', "#" + _this.itemIdPrefix + node.nodeId)
                     .attr('style', 'padding-left:' + leftPadding);
+                // Set Expand and Collapse icones.
                 if (node.nodes) {
                     var treeItemStateIcon = $(templates.treeviewItemStateIcon)
                         .addClass(_this.settings.collapseIcon);
                     treeItem.append(treeItemStateIcon);
                 }
-
+                // set node icon if exist.
+                if (node.icon) {
+                    var treeItemIcon = $(templates.treeviewItemIcon)
+                        .addClass(node.icon);
+                    treeItem.append(treeItemIcon);
+                }
+                // Set node Text.
                 treeItem.append(node.text);
-
+                // Attach node to parent.
                 parentElement.append(treeItem);
-
+                // Build child nodes.
                 if (node.nodes) {
+                    // Node group item.
                     var treeGroup = $(templates.treeviewGroupItem)
-                        .attr('id', 'item-' + node.nodeId);
+                        .attr('id', _this.itemIdPrefix + node.nodeId);
                     parentElement.append(treeGroup);
                     _this.build(treeGroup, node.nodes, depth);
                 }
